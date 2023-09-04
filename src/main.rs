14cfbcca235592c3da8a2799c6f6d42be88743d6
calinanca99 <calinanca99@gmail.com>
2023-09-04 16:17:47 +0300

    Describe routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// The project exposes the following routes:
+// - POST /jwt_token; exchange credentials for JWT token
+// - GET /book/{name}/{author}; looks up a book by book name and
+// author
+// - GET /books; get all the books saved by the user
+// - POST /save_book/{id}; saves a new book
+// - DELETE /delete_book/{id}; deletes a book
+//
+// A valid JWT token must be sent with each request, except
+// POST /jwt_token and GET /book/{name}/{author}.
+
 fn main() {
     println!("Hello, world!");
 }
